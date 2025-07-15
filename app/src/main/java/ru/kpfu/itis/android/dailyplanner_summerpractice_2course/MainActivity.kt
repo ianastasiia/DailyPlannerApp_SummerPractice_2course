e8b317307398effa39260185c8f9ff036a66b42a
ianastasiia <30nastya2005@mail.ru>
2025-07-15 22:53:34 +0300

    Initial commit

---
+++ app/src/main/java/ru/kpfu/itis/android/dailyplanner_summerpractice_2course/MainActivity.kt
@@ -0,0 +1,47 @@
+package ru.kpfu.itis.android.dailyplanner_summerpractice_2course
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.kpfu.itis.android.dailyplanner_summerpractice_2course.ui.theme.DailyPlanner_SummerPractice_2courseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DailyPlanner_SummerPractice_2courseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DailyPlanner_SummerPractice_2courseTheme {
+        Greeting("Android")
+    }
+}
